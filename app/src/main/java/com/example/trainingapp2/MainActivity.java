8c713a2af822fee62ff79f560edd2dda3b56c6e2
mmistes <93210581+mmistes@users.noreply.github.com>
2022-02-24 17:21:53 +0300

    Button fixed

--- app/src/main/java/com/example/trainingapp2/MainActivity.java
+++ app/src/main/java/com/example/trainingapp2/MainActivity.java
@@ -7,16 +7,20 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /* TextView
+        /* // 1 TextView
         setContentView(R.layout.activity_main);
 
         ConstraintLayout constraintLayout = new ConstraintLayout(this);
@@ -54,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
         constraintLayout.addView(textView);
         setContentView(constraintLayout);
 */
-
+        /* // 2 EditText
         setContentView(R.layout.edit_text);
         EditText editText = findViewById(R.id.edit_text1);
 
@@ -76,9 +80,81 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        */
+        /* //3 Button
+        setContentView(R.layout.button);
+       */
+        // Button 2 введение при нажатии
+        setContentView(R.layout.activity_main);
+
+        ConstraintLayout constraintLayout = new ConstraintLayout(this);
+
+        TextView textView = new TextView(this);
+        textView.setId(View.generateViewId());
+        textView.setText("");
+        textView.setTextSize(30);
+        textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        ConstraintLayout.LayoutParams textViewLayout = new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.MATCH_CONSTRAINT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        textViewLayout.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+        textViewLayout.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+        textViewLayout.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
+
+        textView.setLayoutParams(textViewLayout);
+
+
+        EditText editText = new EditText(this);
+        editText.setId(View.generateViewId());
+
+        ConstraintLayout.LayoutParams editTextLayout = new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.MATCH_CONSTRAINT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        editText.setHint("Введите имя");
+        editText.setTextSize(30);
+
+        editTextLayout.topToBottom = textView.getId();
+        editTextLayout.leftToLeft = textView.getId();
+        editTextLayout.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
+
+        editText.setLayoutParams(editTextLayout);
+
+        Button button = new Button(this);
+
+        ConstraintLayout.LayoutParams buttonLayout = new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+
+        button.setText("Ввод");
+        buttonLayout.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+        buttonLayout.topToBottom = editText.getId();
+
+        // Установка OnClickListener
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //обработка нажатия на кнопку
+                textView.setText("Welcome to the GYM, " + editText.getText() + " buddy!");
+
+            }
+        });
+
+        button.setLayoutParams(buttonLayout);
+
+        constraintLayout.addView(textView);
+        constraintLayout.addView(editText);
+        constraintLayout.addView(button);
+
+        setContentView(constraintLayout);
+
 
 
 
 
     }
+
+    public void sendMessage (View view) {
+        TextView textView = findViewById(R.id.get_name);
+        EditText editText = findViewById(R.id.edit_name);
+        textView.setText("Welcome to the GYM, " + editText.getText() + " SLAVE");
+    }
+
+
 }
